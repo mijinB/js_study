cdf01bb1609176cab3202d8292f6ed6bfe8ded38
mijinB <bmj13465@gmail.com>
2023-08-02 13:26:12 +0900

    login form_local storage

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function onLoginSubmit(event) {
   event.preventDefault();
+
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-  greeting.innerText = `Hello ${username}`;
+
+  const userName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+
+  paintGreetings(userName);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+if(savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUserName);
+}
