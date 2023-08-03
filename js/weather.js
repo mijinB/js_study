e75728c1b1372d181f1f59cac1e1cdf1aafab94b
mijinB <bmj13465@gmail.com>
2023-08-03 17:25:14 +0900

    add weather

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "b1da96f5cfcb6152f82d87e31a225a8b"
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
